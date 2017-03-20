784395f829b606dc8ab0be2aba4df9d71df25658
mikejharris <michaelharrisj06@gmail.com>
2017-03-20 16:16:03 -0400

    Created an Ajax call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '77ba8f4e43865505dc7ae126899690e8';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
