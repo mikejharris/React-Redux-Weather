8d7517d44827529fdcf68c44d492d08b3f38d0b4
mikejharris <michaelharrisj06@gmail.com>
2017-03-20 15:45:26 -0400

    Installed the axios library

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '77ba8f4e43865505dc7ae126899690e8';
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,8 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
 
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
