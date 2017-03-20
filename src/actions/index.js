7e801371168c3d54b252fe0225ff24b2668390ed
mikejharris <michaelharrisj06@gmail.com>
2017-03-20 15:43:16 -0400

    wired up the searbar with axios and opw api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '77ba8f4e43865505dc7ae126899690e8';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    return {
+        type: FETCH_WEATHER
+    };
+}
