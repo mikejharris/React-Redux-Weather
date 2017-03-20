aefd8fec2879c0d70d2b168c3542ea2d1060b62d
mikejharris <michaelharrisj06@gmail.com>
2017-03-20 11:48:27 -0400

    Prevent form submit by adding prevent default

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {term: ''};
 
         this.onInputChange = this.onInputChange.bind(this);
     }
-
-    onInputChange(event){
+ 
+    onInputChange(event) {
         this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // We need to go and fetch weather data
     }
 
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
                     placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
